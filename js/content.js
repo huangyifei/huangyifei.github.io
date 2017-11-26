214bfd9dde12488c278fa28ab53a6115bbf3c9a4
huangyifei <huangyifei@corp.netease.com>
2017-11-26 18:21:13 +0800

    内容也改成小写

--- js/content.js
+++ js/content.js
@@ -18,35 +18,35 @@ var content_map = {
 	"china_euro_2017" : {
 		title: "2017中欧（意大利）国际艺术节",
 		subtitle: "发世界之声 铸民族之魂",
-		cover: "images/item/china_euro_2017_bg.JPG",
+		cover: "images/item/china_euro_2017_bg.jpg",
 		content:  '<p>“发世界之声 铸民族之魂”2017 中欧（意大利）国际艺术节将作为全方位综合性展示的艺术窗口，中欧双向沟通互动的高规格平台，于2017年7月15日至22日在意大利卢卡隆重举办。</p>\
-				   <img class="img-responsive img-blog" src="images/item/china_euro_2017_1.JPG" width="100%" alt="" />\
+				   <img class="img-responsive img-blog" src="images/item/china_euro_2017_1.jpg" width="100%" alt="" />\
                    <p>习近平总书记提出 “一带一路”重大倡议，秉着推进“一带一路”文化先行，中欧文化新突破跨国跨地域合作的责任感和使命感，2017中欧（意大利）国际艺术节像一条色彩斑斓的纽带，连接起中欧文化交流新的“生命力”。</p>\
-                   <img class="img-responsive img-blog" src="images/item/china_euro_2017_2.JPG" width="100%" alt="" />\
+                   <img class="img-responsive img-blog" src="images/item/china_euro_2017_2.jpg" width="100%" alt="" />\
                    <p>中国赴意大利交流艺术团体及个人将与欧洲著名交响乐团、指挥合作, 进行跨国合作多场视听盛宴, 将中国的民族音乐真正传递到国际舞台, 为欧洲观众展现中国民族音乐的风采与魅力。</p>\
-                   <img class="img-responsive img-blog" src="images/item/china_euro_2017_3.JPG" width="100%" alt="" />\
+                   <img class="img-responsive img-blog" src="images/item/china_euro_2017_3.jpg" width="100%" alt="" />\
                    <p>让中华民族的文化理念走出国门, 让文化自身说话, 使其成为不同语种、不同地域、不同国家和平交流沟通的媒介。展现中华文化风采, 从而为中国文化的发展营造良好的国际氛围。同时也为中欧艺术团创造了良好的交流契机, 结交国际友谊,为中欧文化真正筑建起新的友谊之桥。</p>\
-                   <img class="img-responsive img-blog" src="images/item/china_euro_2017_4.JPG" width="100%" alt="" />\
+                   <img class="img-responsive img-blog" src="images/item/china_euro_2017_4.jpg" width="100%" alt="" />\
                    <p>中欧（意大利）国际艺术节以国际音乐大师班、中欧国际合唱大赛、中欧国际声乐大赛、中欧国际指挥大赛、中欧艺术教育论坛及中欧演出合作几大板块为主，观摩、探讨并学习欧洲艺术表演教育先进成果及宝贵经验。</p>\
-                   <img class="img-responsive img-blog" src="images/item/china_euro_2017_5.JPG" width="100%" alt="" />\
+                   <img class="img-responsive img-blog" src="images/item/china_euro_2017_5.jpg" width="100%" alt="" />\
                    <p>为中欧校际间的友谊创造了良好的交流契机，参加艺术节的中国教育机构、艺术团体对欧洲多所顶尖院校将进行一对一的交流参访并缔结友好学校，结交国际友谊，为中欧文化真正筑建起新的友谊之桥。同时将举办2017中欧（意大利）国际艺术节交响合唱音乐会，将中国古典名曲《红楼梦》大型交响音乐会奏响欧洲，为中外优秀艺术团体，提供其展示才艺、取长补短、交流融合的机会，将中国的民族音乐真正传递到国际舞台。</p>\
-                   <img class="img-responsive img-blog" src="images/item/china_euro_2017_6.JPG" width="100%" alt="" />\
+                   <img class="img-responsive img-blog" src="images/item/china_euro_2017_6.jpg" width="100%" alt="" />\
                    <p>首届中欧（意大利）国际艺术节是“一带一路”民心相通最好的文化载体，将有效促进“一带一路”沿线各国表演艺术的交流与合作，造福各国人民，推动沿线各国古老文明的共同复兴与繁荣。在文化艺术领域积极落实的务实举措，具有开拓性的历史意义，体现了中欧两国在“一带一路”框架下推动广泛的人文交流与合作的探索和努力。</p>'
 	},
 	"talent-pianst-italy" : {
 		title: "“天赋钢琴家”意大利音乐之旅",
 		subtitle: "",
-		cover: "images/item/talent_pianst_italy_bg.JPG",
+		cover: "images/item/talent_pianst_italy_bg.jpg",
 		content:  '<p>2017 “天赋钢琴家”意大利音乐之旅与意大利维托索&柏坎图国际音乐节深度跨国合作，作为意大利维托索&柏坎图国际音乐节中重要的一部分。</p>\
-				   <img class="img-responsive img-blog" src="images/item/talent_pianst_italy_1.JPG" width="100%" alt="" />\
+				   <img class="img-responsive img-blog" src="images/item/talent_pianst_italy_1.jpg" width="100%" alt="" />\
                    <p>此次音乐之旅受到了意大利卢卡市政府，意大利卢卡市路易吉博凯里尼音乐学院，意大利卢卡市百合花剧院，意大利华人音乐家协会等的大力支持。</p>\
-                   <img class="img-responsive img-blog" src="images/item/talent_pianst_italy_2.JPG" width="100%" alt="" />\
+                   <img class="img-responsive img-blog" src="images/item/talent_pianst_italy_2.jpg" width="100%" alt="" />\
                    <p>在活动期间, 组委会为钢琴学习者提供了一个国际化的学习与交流环境, 与来自世界各地的年轻音乐家相识与交流的独特机会。</p>\
-                   <img class="img-responsive img-blog" src="images/item/talent_pianst_italy_3.JPG" width="100%" alt="" />\
+                   <img class="img-responsive img-blog" src="images/item/talent_pianst_italy_3.jpg" width="100%" alt="" />\
                    <p>此次活动不仅是一次开拓国际视野，提升钢琴技巧， 增进国际音乐界之间交流的绝佳机会, 更是一个展示青年演奏家风采，体验不同文化与艺术灵感碰撞融合的国际舞台。</p>\
-                   <img class="img-responsive img-blog" src="images/item/talent_pianst_italy_4.JPG" width="100%" alt="" />\
+                   <img class="img-responsive img-blog" src="images/item/talent_pianst_italy_4.jpg" width="100%" alt="" />\
                    <p>活动期间，组委会还安排了丰富多彩的游览交流项目, 为来自世界各地的青年音乐家和参与者打开了解多元化文化的窗口。</p>\
-                   <img class="img-responsive img-blog" src="images/item/talent_pianst_italy_5.JPG" width="100%" alt="" />'
+                   <img class="img-responsive img-blog" src="images/item/talent_pianst_italy_5.jpg" width="100%" alt="" />'
 	},
 	"abroad-4" : {
 		title: "2018 万花筒 （西班牙）国际艺术节",
@@ -141,18 +141,18 @@ var content_map = {
 	"domestic-2" : {
 		title: "琴贯古今·致敬经典",
 		subtitle: "第二届上海喜玛拉雅国际钢琴艺术节",
-		cover: "images/item/domestic_2_bg.JPG",
+		cover: "images/item/domestic_2_bg.jpg",
 		content:  '<p>2016年第二届上海喜马拉雅国际钢琴艺术节邀请了来自世界各地优秀的钢琴表演艺术家，来到上海，进行精彩的演出和丰富的交流活动。</p>\
-				   <img class="img-responsive img-blog" src="images/item/domestic_2_1.JPG" width="100%" alt="" />\
+				   <img class="img-responsive img-blog" src="images/item/domestic_2_1.jpg" width="100%" alt="" />\
                    <p>第二届上海喜玛拉雅国际钢琴艺术节暂定于2016年12月3日-12月10日期间在上海喜玛拉雅中心大观舞台举办。</p>\
-                   <img class="img-responsive img-blog" src="images/item/domestic_2_2.JPG" width="100%" alt="" />\
+                   <img class="img-responsive img-blog" src="images/item/domestic_2_2.jpg" width="100%" alt="" />\
                    <p>艺术节期间，来自世界各国优秀的钢琴家将齐聚一堂，在国际一流的音乐舞台上尽情展示，彰显音乐无国界的独特魅力。此外，艺术节还设置了大师讲座、一对一大师课、各国音乐文化展等丰富多彩的活动，让观众们多层次多角度与大师互动、参与到这场艺术盛宴中。</p>\
-                   <img class="img-responsive img-blog" src="images/item/domestic_2_3.JPG" width="100%" alt="" />'
+                   <img class="img-responsive img-blog" src="images/item/domestic_2_3.jpg" width="100%" alt="" />'
 	},
 	"domestic-3" : {
 		title: "波兰拉威尔双钢琴音乐会",
 		subtitle: "从巴洛克音乐到现代音乐，拉威尔双钢琴组合拥有大量四手联弹及双钢琴曲目",
-		cover: "images/item/domestic_3_bg.JPG",
+		cover: "images/item/domestic_3_bg.jpg",
 		content:  '<p>拉威尔双钢琴组合是波兰顶尖双钢琴组合之一。两位钢琴家师从Maja Nosowska，毕业于肖邦音乐学院，之后在德国罗斯托克音乐戏剧学院进修，并于2005年获得最高荣誉学位，受到波兰一代钢琴名家扬·艾凯尔，以色列伊顿与塔米尔等等杰出钢琴家的影响，拉威尔双钢琴组合除了定期在世界各地进行巡演之外，在众多国际钢琴大赛中也崭露头角，包括在捷克耶塞尼克举行的11届国际舒伯特音乐比赛中获一等奖，在意大利罗马举办的瓦伦蒂诺布奇国际音乐比赛中荣获二等奖等等。</p>\
 				   <img class="img-responsive img-blog" src="images/item/domestic_3_1.jpg" width="100%" alt="" />\
                    <p>从巴洛克音乐到现代音乐，拉威尔双钢琴组合拥有大量四手联弹及双钢琴曲目，并且非常具有特色，在双钢琴组合建立的短短时间里，他们与不同室内合唱团及交响乐团合作发行了多张CD, 近期录制的四手联弹作品《瞬息变幻》极具波兰特色，并获得古典届格莱美大奖——波兰古典唱片肖邦大奖。</p>\
@@ -169,7 +169,7 @@ var content_map = {
 	"domestic-4" : {
 		title: "德国钢琴家马丁·柯莱特独奏音乐会",
 		subtitle: "国际约翰内斯·勃拉姆斯大赛和德国全国音乐大赛获奖者",
-		cover: "images/item/domestic_4_bg.JPG",
+		cover: "images/item/domestic_4_bg.jpg",
 		content:  '<p>在过去的几年中，马丁·柯莱特作为钢琴家和室内乐演奏家出现在众多重要的音乐表演场地，声名鹊起。自从斩获了国际约翰内斯·勃拉姆斯大赛（钢琴独奏）和德国全国音乐大赛（与他的单簧管搭档塞巴斯蒂安·曼茨）后，他以成为世界各大音乐节上备受欢迎的座上宾，如卢塞恩音乐节，石勒苏益格-荷尔斯泰因音乐节、 海德堡和梅克伦堡-前波莫瑞州音乐节等等。</p>\
 				   <img class="img-responsive img-blog" src="images/item/domestic_4_1.jpg" width="100%" alt="" />\
                    <p>此外，他还受邀到欧洲各国以及北非等国家进行巡演。作为重要的室内乐演奏家，马丁定期与ARD和ECHO古典回声大奖得主，世界著名交响乐团首席，著名教授等等进行合作并同台演出。</p>\
@@ -184,7 +184,7 @@ var content_map = {
 	"domestic-5" : {
 		title: "比利时青年钢琴家凡·德·威尔第·雅尼克独奏音乐会",
 		subtitle: "维托索&amp;柏坎图国际音乐大师班艺术节冠军",
-		cover: "images/item/domestic_5_bg.JPG",
+		cover: "images/item/domestic_5_bg.jpg",
 		content:  '<p>出生于比利时音乐世家的雅尼克，从小音乐赋予他激情。作为一个钢琴家和音乐人，雅尼克在音乐领域中表现出更多的是热情，细腻与睿智。</p>\
 				   <img class="img-responsive img-blog" src="images/item/domestic_5_1.jpg" width="100%" alt="" />\
                    <p>雅尼克作为一个独奏者及室内乐成员，拥有丰富的演出经历，并和多个著名指挥合作，如艾瑞克·雷德汉德勒，埃蒂安·席本斯，米歇尔·霍夫石坦德等等。多个与他合作的室内乐成员都来自柏林爱乐。</p>\
@@ -194,7 +194,7 @@ var content_map = {
 	"domestic-6" : {
 		title: "奥地利钢琴家科妮丽雅·赫尔曼独奏音乐会",
 		subtitle: "萨尔兹堡的音乐世家，国际巴赫音乐大赛最年轻的优胜者",
-		cover: "images/item/domestic_6_bg.JPG",
+		cover: "images/item/domestic_6_bg.jpg",
 		content:  '<p>科妮丽雅·赫尔曼出生于萨尔兹堡的音乐世家，曾前往维也纳、布达佩斯进修钢琴技巧。她所追随的名师有亚历山大·洛克赫、默里·佩拉希亚、安德拉斯·希夫和阿尔弗雷德·布伦德尔等。</p>\
 				   <img class="img-responsive img-blog" src="images/item/domestic_6_1.jpg" width="100%" alt="" />\
                    <p>1990年，赫尔曼在莱比锡举行的国际巴赫音乐大赛中一举夺魁，成为该项赛事有史以来最年轻的优胜者。1999年，她在萨尔兹堡举行的国际莫扎特音乐大赛中获得特别奖。2002年，她在萨尔兹堡音乐节上首次献技。</p>\
@@ -209,7 +209,7 @@ var content_map = {
 	"domestic-7" : {
 		title: "意大利钢琴家理查德·切凯蒂独奏音乐会",
 		subtitle: "他所创建的意大利“亲密之声”三重奏享誉世界",
-		cover: "images/item/domestic_7_bg.JPG",
+		cover: "images/item/domestic_7_bg.jpg",
 		content:  '<p>拉威尔双钢琴组合是波兰顶尖双钢琴组合之一。两位钢琴家师从Maja Nosowska，毕业于肖邦音乐学院，之后在德国罗斯托克音乐戏剧学院进修，并于2005年获得最高荣誉学位，受到波兰一代钢琴名家扬·艾凯尔，以色列伊顿与塔米尔等等杰出钢琴家的影响，拉威尔双钢琴组合除了定期在世界各地进行巡演之外，在众多国际钢琴大赛中也崭露头角，包括在捷克耶塞尼克举行的11届国际舒伯特音乐比赛中获一等奖，在意大利罗马举办的瓦伦蒂诺布奇国际音乐比赛中荣获二等奖等等。</p>\
 				   <img class="img-responsive img-blog" src="images/item/domestic_7_1.jpg" width="100%" alt="" />\
                    <p>从巴洛克音乐到现代音乐，拉威尔双钢琴组合拥有大量四手联弹及双钢琴曲目，并且非常具有特色，在双钢琴组合建立的短短时间里，他们与不同室内合唱团及交响乐团合作发行了多张CD, 近期录制的四手联弹作品《瞬息变幻》极具波兰特色，并获得古典届格莱美大奖——波兰古典唱片肖邦大奖。</p>\
@@ -222,7 +222,7 @@ var content_map = {
 	"domestic-8" : {
 		title: "鲍里斯·贝尔曼独奏音乐会",
 		subtitle: "当代钢琴作品演绎的大家、俄罗斯钢琴学派的代表人物、以色列钢琴家、美国钢琴家",
-		cover: "images/item/domestic_8_bg.JPG",
+		cover: "images/item/domestic_8_bg.jpg",
 		content:  '<p>业界给鲍里斯·贝尔曼戴上了各种桂冠：“当代钢琴作品演绎的大家、俄罗斯钢琴学派的代表人物、以色列钢琴家、美国钢琴家”等等。我们非常荣幸的邀请到鲍里斯·贝尔曼先生担任“天赋钢琴家”大赛国际评委会名誉主席</p>\
 				   <img class="img-responsive img-blog" src="images/item/domestic_8_1.png" width="100%" alt="" />\
                    <p>无论作为现任耶鲁大学音乐学院钢琴系主任还是在世界各大著名钢琴比赛（如英国的里兹国际钢琴大赛、爱尔兰的都柏林国际钢琴大赛、以色列的鲁宾斯坦钢琴大赛） 中担任评委，鲍里斯·贝尔曼都是一个标识，一个世界顶级大师的标识。</p>\
@@ -232,7 +232,7 @@ var content_map = {
                    <p>他与皇家大会堂管弦乐团、布商大厦乐团、伦敦爱乐乐团、多伦多交响乐团、以色列爱乐乐团、明尼苏达交响乐团、底特律交响乐团、休斯顿交响乐团、亚特兰大交响乐团、圣彼得堡爱乐乐团、苏格兰皇家乐团的合作演出广受赞誉。他还在万宝路、滑铁卢、拉维尼亚和以色列等等一系列音乐节上演出。</p>\
                    <img class="img-responsive img-blog" src="images/item/domestic_8_4.jpg" width="100%" alt="" />\
                    <p>贝尔曼的钢琴演绎融贯古今，他更是俄罗斯当代钢琴作品的倡导者，许多当代作曲家的钢琴作品如施尼特凯、特尼索夫、勋伯格、斯特克豪森、贝利奥、利盖蒂的当代钢琴作品都是由他在俄罗斯首演。他是斯克里亚宾、斯特拉文斯基、拉赫玛尼诺夫和普罗科菲耶夫等俄罗斯作曲家钢琴作品的权威诠释者。</p>\
-                   <img class="img-responsive img-blog" src="images/item/domestic_8_5.JPG" width="100%" alt="" />'
+                   <img class="img-responsive img-blog" src="images/item/domestic_8_5.jpg" width="100%" alt="" />'
 	},
 	"domestic-9" : {
 		title: "国际钢琴盛典-天赋钢琴家大赛",
@@ -284,13 +284,13 @@ var content_map = {
 	"domestic-10" : {
 		title: "荷兰阿姆斯特丹木管五重奏",
 		subtitle: "“最传神的、最迷人的”木管五重奏",
-		cover: "images/item/domestic_10_bg.JPG",
+		cover: "images/item/domestic_10_bg.jpg",
 		content:  '<p>阿姆斯特丹木管五重奏的五位音乐家对音乐、曲目的创新有着非常高的要求，和非传统方式演绎的共同追求下，建立了阿姆斯特丹木管五重奏。他们每年都会推出全新的五重奏、三重奏和四重奏木管曲目。由于他们不但与深谙古典音乐的听众分享美妙之外，而且还擅长令那些不甚了解古典音乐的听众就此爱上木管乐器，因此，他们的音乐会被听众和评论界称为“最传神的、最迷人的”。他们还积极参与各类面向儿童和普及古典音乐的教育性音乐会，以强大表现力和现场感染力吸引了荷兰及欧洲各地的歌唱家、作曲家和剧场导演与之合作。</p>\
-				   <img class="img-responsive img-blog" src="images/item/domestic_10_1.JPG" width="100%" alt="" />\
+				   <img class="img-responsive img-blog" src="images/item/domestic_10_1.jpg" width="100%" alt="" />\
                    <p>阿姆斯特丹五重奏的演出足迹遍布荷兰及欧洲各地，特别是阿姆斯特丹皇家音乐厅、鹿特丹DeDoelen音乐厅、奈美根卢克斯剧院。他们的演出定期由荷兰国家古典音乐电台直播。他们曾参加奥兰多音乐节、荷兰古典音乐节、Gelderse夏季音乐节等。</p>\
                    <p>2007年，他们还有幸受到荷兰女王的邀请为皇室演出。他们的出色音乐表现受到现代作曲家的关注，曾受邀首演荷兰作曲家亨利•凯德、约翰•德维特和约克•范德尔等人作品。</p>\
                    <p>“他们的声音统一、和谐，屏息聆听下可分辨那些色彩层次丰富的细微差别。他们是五个声音的魔术师！”</p>\
-                   <img class="img-responsive img-blog" src="images/item/domestic_10_2.JPG" width="100%" alt="" />\
+                   <img class="img-responsive img-blog" src="images/item/domestic_10_2.jpg" width="100%" alt="" />\
                    <h3>蒂姆•文特索尔——长笛</h3>\
                    <img class="img-responsive img-blog" src="images/item/domestic_10_5.jpg" width="100%" alt="" />\
                    <p>蒂姆7岁起在德国学习长笛。1998年以优异成绩进入海牙皇家音乐学院，师从名师荷兰长笛家里恩•德海德（Rien de Reede）、蒂斯•霍尔达（ThiesRoorda）和威尔士长笛演奏家艾米丽•贝南（Emily Beynon），最终取得硕士学位。他还曾在2001至2002年以全额奖学金赴英国进修。</p>\
@@ -301,14 +301,14 @@ var content_map = {
                    <p>玛莱伊幼年入选阿姆斯特丹音乐学院“天赋少年”课程，后入海牙皇家音乐学院师从阿里•格伦（Ali Groen）和鲍琳•奥斯顿里克（Pauline Oostenrijk）。21岁时受著名双簧管演奏家罗兰•派赫诺（Roland Perrenoud）及毛里斯•布赫格（Maurice Bourgue）邀请赴日内瓦音乐学院深造，2008年以满分获得演奏证书。</p>\
                    <p>她曾参加过世界知名双簧管演奏家克劳斯•贝克（Klaus Becker）、欧玛•佐伯利（Omar Zoboli）、巴特•施尼曼（Bart Schneeman）和保罗•古德曼（Bart Schneeman）大师课。她经常受邀与海牙爱乐乐团、林伯格交响乐团、荷兰小交响乐团和北荷兰乐团合作。她曾获得1998年Dubbelriet大赛、2002年克里斯蒂娜公主大赛和2005年意大利国际管乐器比赛奖项。这些奖项为她赢得在荷兰、意大利各重要音乐场地的演出，其中包括阿姆斯特丹皇家音乐大厅独奏音乐会。</p>\
                    <h3>汉卡•卡罗拉·苏菲·克鲁特——单簧管</h3>\
-                   <img class="img-responsive img-blog" src="images/item/domestic_10_7.JPG" width="100%" alt="" />\
+                   <img class="img-responsive img-blog" src="images/item/domestic_10_7.jpg" width="100%" alt="" />\
                    <p>汉卡自幼擅长钢琴和单簧管两种乐器，在阿姆斯特丹皇家音乐学院学习单簧管后，曾参加德国演奏家拉尔夫•马努、卡尔•莱斯特、比利时演奏家罗兰德•亨德里克斯和布里安•波拉德等世界单簧管名家大师课。</p>\
                    <p>她目前任北荷兰交响乐团第二单簧管。同时，她经常受邀在荷兰爱乐乐团、荷兰室内乐团、荷兰广播爱乐乐团、荷兰广播室内乐团、荷兰小交响乐团、海牙爱乐乐团、荷兰皇家海军大乐团、荷兰皇家空军乐团等众多乐团合作，担任独奏或替补单簧管首席。她曾在著名指挥家弗兰茨•与布鲁根（FruansBruggen）、雅普•冯茨威顿（Jaap van Zweden）、兰贝特•德李由（Reinbert de Leeuw）和尼姆•雅尔维（NeemeJarvi）执棒下担任独奏。</p>\
                    <h3>马丁•安德里亚· 阿佩洛——法国号</h3>\
                    <img class="img-responsive img-blog" src="images/item/domestic_10_8.jpg" width="100%" alt="" />\
                    <p>马丁八岁时，开始了学习法国号的生涯，15岁便成为国家青年管乐团一员，2014年以优等生的名号毕业并获得奖学金，并师从Paul van Zelm继续在科隆音乐艺术学院进修。 在学习期间，马丁也是皇家大会堂学院管弦乐团一员，他也与荷兰著名交响乐团鹿特丹爱乐以及皇家大会堂管弦乐团合作. 当然他常被邀约和著名指挥合作，例如Valery Gergiev, Yannick Nezet-Seguin, Sir Mark Elder 等等。</p>\
                    <h3>玛莱伊可·盖特杰·杰斯特拉——巴松管</h3>\
-                   <img class="img-responsive img-blog" src="images/item/domestic_10_9.JPG" width="100%" alt="" />\
+                   <img class="img-responsive img-blog" src="images/item/domestic_10_9.jpg" width="100%" alt="" />\
                    <p>玛莱伊可1985年出生于阿姆斯特丹，12岁开始专业巴松管演奏。她曾分别在荷兰海牙师从约翰•斯坦曼（Johan Steinmann）、在马德里拜师克劳斯•图纳曼（Klaus Thunemann），在柏林师从沃尔卡•塔斯曼（Volker Tessmann），并在乌勒•克里斯蒂安•代勒（Ole Kristian Dahl）指导下获硕士学位。</p>\
                    <p>玛莱伊可曾是荷兰国家青年交响乐团和欧盟青年乐团成员。她曾受邀以独奏身份与布莱梅爱乐室内乐团、杜塞尔多夫莱茵歌剧院乐团、苏黎世音乐厅管弦乐团、荷兰海牙管弦乐团、新欧洲乐团等合作演出维瓦尔第和莫扎特的协奏曲。</p>'
 	},
