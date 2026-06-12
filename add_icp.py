f03d1e4f21117a00446fb6f39d4b9d24d81078ae
Huangyifei <bupt.huangyifei@gmail.com>
2026-06-12 15:07:01 +0800

    add ICP

--- add_icp.py
+++ add_icp.py
@@ -7,8 +7,8 @@ old_text ='NexT.Muse'
 new_text ='主体备案号：京ICP备16052131号'
 old_text1 ='技术大杂烩'
 new_text1 ='个人技术经验点滴积累'
-old_text2 ='黄怡菲的博客'
-new_text2 ='互联网技术大杂烩'
+old_text2 ='互联网技术大杂烩'
+new_text2 ='技术大杂烩2022'
 old_text3 ='主题 -'
 new_text3 =''
 
@@ -17,11 +17,11 @@ for (dirpath, dirnames, filenames) in os.walk('.'):
         if filename.endswith('.html'):
             f=open(os.path.join(dirpath,filename), 'r');
             content=f.read()
-            newcontent=content.replace(old_url,new_url)
-            newcontent=newcontent.replace(old_text,new_text)
-            newcontent=newcontent.replace(old_text1,new_text1)
-            newcontent=newcontent.replace(old_text2,new_text2)
-            newcontent=newcontent.replace(old_text3,new_text3)
+            newcontent=content.replace(old_text2,new_text2)
+#            newcontent=newcontent.replace(old_text,new_text)
+#            newcontent=newcontent.replace(old_text1,new_text1)
+#            newcontent=newcontent.replace(old_text2,new_text2)
+#            newcontent=newcontent.replace(old_text3,new_text3)
             f.close()
             f=open(os.path.join(dirpath,filename), 'w');
             f.write(newcontent)
